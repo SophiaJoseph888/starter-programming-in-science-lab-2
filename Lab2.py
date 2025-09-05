1c4c43fecc6e12a0b07e91618c9fd0c79d407096
SophiaJoseph888 <sophiajoseph8888@gmail.com>
2025-09-05 15:43:51 -0400

    Update Lab2.py
    attempt 1

--- Lab2.py
+++ Lab2.py
@@ -3,10 +3,14 @@
 # Round up to one decimal point
 def calculate_height(h0, t):
     # TODO: Implement this function
-    pass  # Replace with your code
-
+    g=9.8
+    h=h0-0.5*g*t*t
+    
 # Function 2: Calculate the distance traveled by the car
 # This function should take the time t as input and return the distance traveled by the car.
 def calculate_car_distance(t):
     # TODO: Implement this function
     pass  # Replace with your code
+    speed=60 #m/s
+    distance=speed*t
+    return distance
